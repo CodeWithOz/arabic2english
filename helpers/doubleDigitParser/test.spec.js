89295da9c7d7ef395a2d35b27eeb18d4e3774037
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-12 16:38:09 +0100

    Test if getFirstDigit exists

--- helpers/doubleDigitParser/test.spec.js
+++ helpers/doubleDigitParser/test.spec.js
@@ -1,5 +1,11 @@
-import parseDouble from './';
+import parseDouble, { getFirstDigit } from './';
 
 test('parseDouble is a function', () => {
   expect(typeof parseDouble).toEqual('function');
+});
+
+describe('getFirstDigit', () => {
+  test('is a function', () => {
+    expect(typeof getFirstDigit).toEqual('function');
+  });
 });
