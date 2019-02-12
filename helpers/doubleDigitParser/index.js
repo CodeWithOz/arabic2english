deeef73951c5dbe11adff341e24519b8a4979b72
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-12 19:35:42 +0100

    Create getSecondDigit

--- helpers/doubleDigitParser/index.js
+++ helpers/doubleDigitParser/index.js
@@ -2,6 +2,8 @@ export function getFirstDigit(strNum) {
   return strNum[0];
 }
 
+export function getSecondDigit() {}
+
 export function handleLeadingZeros() {}
 
 export default function parseDouble() {}
