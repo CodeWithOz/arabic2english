7c4d354e6c80769a3e4d7a55484a72ebef71b1d6
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-12 16:19:01 +0100

    Test if it parses single digits correctly

--- helpers/singleDigitParser.test.js
+++ helpers/singleDigitParser.test.js
@@ -3,3 +3,45 @@ import parseSingle from './singleDigitParser';
 test('parseSingle is a function', () => {
   expect(typeof parseSingle).toEqual('function');
 });
+
+describe('parseSingle returns the correct name for', () => {
+  test('0', () => {
+    expect(parseSingle('0')).toEqual('zero');
+  });
+
+  test('1', () => {
+    expect(parseSingle('1')).toEqual('one');
+  });
+
+  test('2', () => {
+    expect(parseSingle('2')).toEqual('two');
+  });
+
+  test('3', () => {
+    expect(parseSingle('3')).toEqual('three');
+  });
+
+  test('4', () => {
+    expect(parseSingle('4')).toEqual('four');
+  });
+
+  test('5', () => {
+    expect(parseSingle('5')).toEqual('five');
+  });
+
+  test('6', () => {
+    expect(parseSingle('6')).toEqual('six');
+  });
+
+  test('7', () => {
+    expect(parseSingle('7')).toEqual('seven');
+  });
+
+  test('8', () => {
+    expect(parseSingle('8')).toEqual('eight');
+  });
+
+  test('9', () => {
+    expect(parseSingle('9')).toEqual('nine');
+  });
+});
