51f80b6ffb088b2f3332789293fbe27ad59e7f1a
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 12:22:50 +0100

    Refactor functions to use handleLast2digits

--- helpers/tripleDigitParser/index.js
+++ helpers/tripleDigitParser/index.js
@@ -13,18 +13,15 @@ export function handleLast2digits(strNum) {
 // this function will be called after the first digit has been
 // confirmed to be zero
 export function handleLeadingZeros(strNum) {
-  // refactor these two steps into a separate function
-  const last2digits = removeFirstDigit(strNum);
-  return parseDouble(last2digits);
+  return handleLast2digits(strNum);
 }
 
 // this function will be called after confirming that the leading
 // digit is non-zero
 export function getHundredName(strNum) {
   const firstDigit = getFirstDigit(strNum);
-  const last2digits = removeFirstDigit(strNum);
-  const last2digitsName = parseDouble(last2digits);
   const hundredName = `${parseSingle(firstDigit)} hundred`;
+  const last2digitsName = handleLast2digits(strNum);
   return `${hundredName}${
     last2digitsName !== '' ? ` and ${last2digitsName}` : ''
   }`;
