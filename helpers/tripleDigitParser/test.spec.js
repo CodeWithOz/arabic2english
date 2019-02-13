a48a1a0c36458db9925953de2cd0123a73d3f8ee
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 05:37:21 +0100

    Test if handleLeadingZeros of parseTriple exists

---
+++ helpers/tripleDigitParser/test.spec.js
@@ -0,0 +1,7 @@
+import { handleLeadingZeros } from './';
+
+describe('handleLeadingZeros', () => {
+  test('is a function', () => {
+    expect(typeof handleLeadingZeros).toEqual('function');
+  });
+});
