1f26d4bf0a6742fbe657e6844e7007b8272643cc
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 16:00:05 +0100

    Implement getSetName functionality

--- helpers/largeNumbersParser/index.js
+++ helpers/largeNumbersParser/index.js
@@ -2,4 +2,14 @@ export function getLast3chars(strNum) {
   return strNum.slice(-3);
 }
 
-export function getSetName() {}
+const setsMap = {
+  1: '',
+  2: 'thousand',
+  3: 'million',
+  4: 'billion',
+  5: 'trillion',
+  6: 'quadrillion'
+};
+export function getSetName(setIndex) {
+  return setsMap[setIndex];
+}
