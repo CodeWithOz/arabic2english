a680c05c6fc9f77b7964dc234c1146afa949ec6c
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 06:00:17 +0100

    Test if handleLeadingZeros handles 00*
    This is for non-zero endings.

--- helpers/tripleDigitParser/test.spec.js
+++ helpers/tripleDigitParser/test.spec.js
@@ -8,4 +8,10 @@ describe('handleLeadingZeros', () => {
   test('returns an empty string for 000', () => {
     expect(handleLeadingZeros('000')).toEqual('');
   });
+
+  test('correctly names 00* with non-zero endings', () => {
+    expect(handleLeadingZeros('002')).toEqual('two');
+    expect(handleLeadingZeros('003')).toEqual('three');
+    expect(handleLeadingZeros('004')).toEqual('four');
+  });
 });
