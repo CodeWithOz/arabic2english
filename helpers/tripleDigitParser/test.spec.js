570dac7ea07af943b7a96acc73437d3da1586fc1
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 05:42:00 +0100

    Test if handleLeadingZeros handles 000

--- helpers/tripleDigitParser/test.spec.js
+++ helpers/tripleDigitParser/test.spec.js
@@ -4,4 +4,8 @@ describe('handleLeadingZeros', () => {
   test('is a function', () => {
     expect(typeof handleLeadingZeros).toEqual('function');
   });
+
+  test('returns an empty string for 000', () => {
+    expect(handleLeadingZeros('000')).toEqual('');
+  });
 });
