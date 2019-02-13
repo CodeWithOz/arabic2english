9b4b4c3e060b7393c24166e9fa2419c12d379536
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 15:46:30 +0100

    Test if strings less than 4 chars are handled

--- helpers/largeNumbersParser/test.spec.js
+++ helpers/largeNumbersParser/test.spec.js
@@ -4,4 +4,10 @@ describe('getLast3chars', () => {
   test('is a function', () => {
     expect(typeof getLast3chars).toEqual('function');
   });
+
+  test('gets all chars when length is 3 or less', () => {
+    expect(getLast3chars('010')).toEqual('010');
+    expect(getLast3chars('23')).toEqual('23');
+    expect(getLast3chars('5')).toEqual('5');
+  });
 });
