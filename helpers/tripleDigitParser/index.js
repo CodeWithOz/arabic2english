75a75af577b4ce4edd678534c56152dc89adcfcf
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 05:56:55 +0100

    Handle 000
    This can now be implemented due to the added functionality
    in parseDouble.

--- helpers/tripleDigitParser/index.js
+++ helpers/tripleDigitParser/index.js
@@ -1,1 +1,7 @@
-export function handleLeadingZeros() {}
+import parseDouble from '../doubleDigitParser';
+
+// this function will be called after the first digit has been
+// confirmed to be zero
+export function handleLeadingZeros(strNum) {
+  return parseDouble(strNum);
+}
