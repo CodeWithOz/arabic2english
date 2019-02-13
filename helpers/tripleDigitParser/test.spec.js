2e65bafc4cff0ae076f88586c71518eba82b6759
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 06:17:14 +0100

    Test if handleLeadingZeros handles 0**
    This is for numbers where the middle digit is not zero.
    The test already passes due to the solution used
    to pass a previous test.

--- helpers/tripleDigitParser/test.spec.js
+++ helpers/tripleDigitParser/test.spec.js
@@ -14,6 +14,12 @@ describe('handleLeadingZeros', () => {
     expect(handleLeadingZeros('003')).toEqual('three');
     expect(handleLeadingZeros('004')).toEqual('four');
   });
+
+  test('correctly names 0** with non-zero mid number', () => {
+    expect(handleLeadingZeros('016')).toEqual('sixteen');
+    expect(handleLeadingZeros('040')).toEqual('forty');
+    expect(handleLeadingZeros('075')).toEqual('seventy-five');
+  });
 });
 
 describe('removeFirstDigit', () => {
