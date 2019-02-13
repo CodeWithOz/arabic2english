490f80513175b73a33854c6f8b97939b35ca4b8b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-13 06:03:57 +0100

    Handle 00* in handleLeadingZeros

--- helpers/tripleDigitParser/index.js
+++ helpers/tripleDigitParser/index.js
@@ -3,5 +3,6 @@ import parseDouble from '../doubleDigitParser';
 // this function will be called after the first digit has been
 // confirmed to be zero
 export function handleLeadingZeros(strNum) {
-  return parseDouble(strNum);
+  const last2digits = strNum.slice(1);
+  return parseDouble(last2digits);
 }
