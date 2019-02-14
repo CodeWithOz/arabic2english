8f97d8d0839826f5b6e3c7c95e53839e12e6f2d3
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-14 14:42:13 +0100

    Remove cli argument converter

--- arabic2english.test.js
+++ arabic2english.test.js
@@ -1,5 +1,4 @@
 import converter from './arabic2english';
-import cliArgToNum from './helpers/convertCliArgToNum';
 
 test('converter is a function', () => {
   expect(typeof converter).toEqual('function');
--- helpers/convertCliArgToNum.js
+++
@@ -1,3 +0,0 @@
-export default function cliArgToNum(strNum) {
-  return Number(strNum);
-}
