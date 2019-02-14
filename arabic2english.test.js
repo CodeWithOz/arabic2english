51379ccb6d9e19684bdeb6a1da356b6c7d530872
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-14 14:46:50 +0100

    Test if converter handles zero-only digits

--- arabic2english.test.js
+++ arabic2english.test.js
@@ -4,4 +4,9 @@ describe('converter', () => {
   test('is a function', () => {
     expect(typeof converter).toEqual('function');
   });
+
+  test('correctly names 0', () => {
+    expect(converter('0')).toEqual('zero');
+    expect(converter('00000000')).toEqual('zero');
+  });
 });
