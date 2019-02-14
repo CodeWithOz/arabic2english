1d6609f41ecec7ccdf96fd0d0ee701557815cc84
Uche Ozoemena <ucheozoemena@gmail.com>
2019-02-14 16:14:04 +0100

    Implement functionality of converter

--- arabic2english.js
+++ arabic2english.js
@@ -1,5 +1,19 @@
-import { isZero } from './helpers';
+import { isZero, getFirstDigit, removeFirstDigit } from './helpers';
+import parseLarge from './helpers/largeNumbersParser';
 
 export default function converter(strNum) {
   if (isZero(strNum)) return 'zero';
+
+  let isNegative = false,
+    name = '';
+
+  if (getFirstDigit(strNum) === '-') {
+    isNegative = true;
+    name = 'minus ';
+    strNum = removeFirstDigit(strNum);
+  }
+
+  name += parseLarge(strNum);
+
+  return name;
 }
